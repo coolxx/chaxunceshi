bcda02ad0dbdf3e351c2169d3ac82fb860b557f7
henghenghahadada <xing19931122@sina.com>
2018-01-25 13:48:07 +0800

    sad
    ff

---
+++ js/index.js
@@ -0,0 +1,20 @@
+$(function () {
+    $('.m2-btn p').each(function (index) {
+        $(this).click(function () {
+            $('.m2-btn p').removeClass('on2').eq(index).addClass('on2');
+            $('.m2-show').hide().eq(index).show()
+        })
+    })
+    $('.m2-two-btn li').each(function (index) {
+        $(this).click(function () {
+            $('.m2-two-btn li').removeClass('on3').eq(index).addClass('on3');
+            $(this).parents('.m2-show').children('a').hide().eq(index).css('display','block')
+        })
+    })
+    $('.m2-two-btn2 li').each(function (index) {
+        $(this).click(function () {
+            $('.m2-two-btn2 li').removeClass('on4').eq(index).addClass('on4');
+            $(this).parents('.m2-show').children('a').hide().eq(index).css('display','block')
+        })
+    })
+})
